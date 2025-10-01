865e747931816d95194c31f9ef3137b18cf790e6
Bhatti <128886244+A-Rehman3737@users.noreply.github.com>
2025-10-01 19:41:31 +0500

    Update login.tsx
    second commit

--- Clockify Sysovo/frontend/src/component/login.tsx
+++ Clockify Sysovo/frontend/src/component/login.tsx
@@ -118,7 +118,7 @@ export default function LoginForm() {
             fontSize: "13px"
           }}
         >
-          Sign in to Sysovo
+          Sign in to ovo
         </Typography>
 
         {error && (
